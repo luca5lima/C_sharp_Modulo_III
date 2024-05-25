7dece58be861f4402994a7524cb20e0a94228f71
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-05-25 18:16:28 -0300

    Expressões boolianas
    Usando vários operadores e técnicas, você escreveu um código que avaliou a equivalência de valores: você comparou valores para ver se um era maior, menor ou igual ao outro. Você comparou as cadeias de caracteres, manipulando-as para criar uma comparação mais precisa que eliminasse problemas como maiúsculas e minúsculas ou espaços em branco que pudessem interferir no resultado esperado. Você aprendeu a usar a negação lógica para inverter um valor na comparação, e usou o resultado de métodos nas expressões boolianas. Por fim, você usou o operador condicional para avaliar sucintamente uma condição e retornar um resultado.

---
+++ CsharpModulo3/Project1/Program.cs
@@ -0,0 +1,80 @@
+﻿string permission = "Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    string result = level > 55 ?  "Welcome, Super Admin user." : "Welcome, Admin user.";
+    Console.WriteLine(result);
+
+}
+else if (permission.Contains("Manager"))
+{
+    string result = level >= 20 ?  "Contact an Admin for access.\n" : "You do not have sufficient privileges.\n";
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+// ------------------------------------------------------------------------
+
+// Console.WriteLine("a" == "a");
+// Console.WriteLine("a" == "A");
+// Console.WriteLine(1 == 2);
+
+// string myValue = "a";
+// Console.WriteLine(myValue == "a");
+// ------------------------------------------------------------------------
+
+// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+// ------------------------------------------------------------------------
+
+// Console.WriteLine("a" != "a");
+// Console.WriteLine("a" != "A");
+// Console.WriteLine(1 != 2);
+
+// string myValue = "a";
+// Console.WriteLine(myValue != "a");
+// ------------------------------------------------------------------------
+
+// Console.WriteLine(1 > 2);
+// Console.WriteLine(1 < 2);
+// Console.WriteLine(1 >= 1);
+// Console.WriteLine(1 <= 1);
+// ------------------------------------------------------------------------
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(!pangram.Contains("fox"));
+// Console.WriteLine(!pangram.Contains("cow"));
+// ------------------------------------------------------------------------
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(!pangram.Contains("fox"));
+// Console.WriteLine(!pangram.Contains("cow"));
+// ------------------------------------------------------------------------
+
+// int a = 7;
+// int b = 6;
+// Console.WriteLine(a != b); // output: True
+// string s1 = "Hello";
+// string s2 = "Hello";
+// Console.WriteLine(s1 != s2); // output: False
+
+// #############################operador condicional############################
+
+// int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {discount}\n");
+// ------------------------------------------------------------------------
+
+// int saleAmount = 10;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {discount}\n");
+// ------------------------------------------------------------------------
+
+// Random coin = new Random();
+// int flip = coin.Next(0,2);
+// Console.WriteLine((flip == 0) ?  "Heads" : "Tails\n");
+
